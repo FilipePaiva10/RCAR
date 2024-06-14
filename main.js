578c12cffad59aa8f91d46f3065d536617b902a3
arthur-asilva <icaro.arthur66@gmail.com>
2024-06-14 08:54:37 -0300

    add csp in main.js

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(function (req, res, next) {
+    res.setHeader(
+      'Content-Security-Policy',
+      "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+    );
+    next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/ada/', async(req, res) => {
